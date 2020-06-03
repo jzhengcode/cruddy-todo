3562cc9040c1e74517c579c4a5919fc92762258a
Janet Zheng <janet.zhengg@gmail.com>
2020-06-02 22:27:13 -0700

    cleaned up code

--- datastore/index.js
+++ datastore/index.js
@@ -3,100 +3,80 @@ const path = require('path');
 const _ = require('underscore');
 const counter = require('./counter');
 
-// could we be using items as RAM and writing the file as storage
-// var items = {};
-
 // Public API - Fix these CRUD functions ///////////////////////////////////////
 let newPath = (fileName) => (path.join(exports.dataDir, `${fileName}.txt`));
 
 exports.create = (text, callback) => {
   counter.getNextUniqueId((err, id) => {
-    if (err){
-      // let's come back here later
+    if (err) {
       callback(err);
     } else {
       var newFile = newPath(id);
       fs.writeFile(newFile, text, (err) => {
-      if (err) {
-        callback(err);
-      } else {
-        callback(null, { id, text });
-      }
+        if (err) {
+          callback(err);
+        } else {
+          callback(null, {id, text});
+        }
       });
-
     }
-
-});
-
+  });
 };
 
 
 
 exports.readAll = (callback) => {
-  // use fs.readdir to get a list of all files
-    //take each file store it in an array
-let data=[];
-fs.readdir(exports.dataDir,(err,files) =>{
-  if(err) {
-    callback(err);
-  } else {
-    for(var i=0; i<files.length; i++){
 
-      data.push({id: files[i].substr(0,5), text: files[i].substr(0,5)})
+  let data = [];
+  fs.readdir(exports.dataDir, (err, files) => {
+    if (err) {
+      callback(err);
+    } else {
+      for (var i = 0; i < files.length; i++) {
+
+        data.push({id: files[i].substr(0, 5), text: files[i].substr(0, 5)});
+      }
+
+      callback(null, data);
     }
-
-    callback(null, data);
-  }
-})
-
-  // var data = [];
-  // data.push(fs.readdir((exports.dataDir,(err, data) => {
-  //   if (err) {
-  //     throw ('error reading all');
-  //   } else {
-  //     callback(null, data);
-  //   }
-  // })))
-  // console.log(data);
-
+  });
 
 };
 
 exports.readOne = (id, callback) => {
   //use readfile
-  var searchDir = newPath(id)
-  fs.readFile(searchDir, 'utf8', (err, todoText)=>{
-    if(err){
-      callback(err)
+  var searchDir = newPath(id);
+  fs.readFile(searchDir, 'utf8', (err, todoText)=> {
+    if (err) {
+      callback(err);
     } else {
-      if (!todoText){
+      if (!todoText) {
         callback(new Error(`No item with id: ${id}`));
       } else {
         callback(null, {id, text: todoText});
       }
 
     }
-  })
+  });
 
 };
 
 exports.update = (id, text, callback) => {
 
-
-  let searchDir = newPath(id)
-  fs.readFile(searchDir, (err)=>{
-    if(err){
-      callback(err)
-    }else{
-      fs.writeFile(searchDir, text, (err)=>{
-        if(err){
-          callback(err)
-        }else{
-          callback(null, {id, text})
+  let searchDir = newPath(id);
+  fs.readFile(searchDir, (err) => {
+    if (err) {
+      callback(err);
+    } else {
+      fs.writeFile(searchDir, text, (err) => {
+        if (err) {
+          callback(err);
+        } else {
+          callback(null, {id, text});
         }
-      })
+      });
     }
-  })
+  });
 
 };
 
@@ -105,12 +85,12 @@ exports.delete = (id, callback) => {
 
   let searchDir = newPath(id);
   fs.unlink(searchDir, (err) => {
-    if(err){
+    if (err) {
       callback(err);
-    }else{
+    } else {
       callback();
     }
-  })
+  });
 
 };
 
